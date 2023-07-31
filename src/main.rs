eca589b97ca60662f62d4fa66caaaf9c05c2b960
Efe İzbudak <efe.izbudak@metu.edu.tr>
2023-07-31 20:18:38 +0300

    async

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,56 @@
-use reqwest::blocking::Client;
+use colored::Colorize;
 use scraper::{Html, Selector};
 use std::cmp::min;
 use std::io::{stdin, stdout, Write};
 use std::process::Command;
-// use tokio;
-use colored::Colorize;
 
-// parse an argument that will enable quick mode to play the first pick
-// parse a default argument that takes a youtube link
-// parse an argument to make a search string
-
-// #[tokio::main]
-fn main() {
-    let client = Client::new();
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::new();
     loop {
         print!("\x1B[2J\x1B[1;1H");
         print!("{}", "$ ".truecolor(255, 255, 255));
         stdout().flush().unwrap();
         let mut search = String::new();
         stdin().read_line(&mut search).expect("Filed to read line");
-        if &search.trim() == &"q" {
+        if search.trim() == "q" {
             break;
         } else if &search.trim()[..4] == "http" {
             let split: Vec<&str> = search.trim().split("?v=").collect();
             let id = split[1];
             Command::new("mpv")
                 .arg(format!(
-                    "https://vid.puffyan.us/latest_version?id={}&itag=251&local=true",
+                    "https://iv.melmac.space/latest_version?id={}&itag=251&local=true",
                     id
                 ))
                 .status()
                 .expect("Process failed.");
-        } else if &search.trim() == &"" {
+        } else if search.trim() == "" {
             continue;
         } else {
             let response = client
                 .get(format!(
-                    "https://vid.puffyan.us/search?q={}&type=video",
+                    "https://iv.melmac.space/search?q={}&type=video",
                     search
                 ))
                 .send()
+                .await
                 .unwrap()
                 .text()
+                .await
                 .unwrap();
             let document = Html::parse_document(&response);
-            let title_selector = Selector::parse("div.h-box>a>p").unwrap();
-            let titles: Vec<String> = document
-                .select(&title_selector)
-                .map(|x| x.inner_html())
-                .collect();
-            let url_selector = Selector::parse("div.pure-u-md-1-4>div.h-box>a").unwrap();
+            let selector = Selector::parse("div.video-card-row>a").unwrap();
+            let titles: Vec<&str> = document.select(&selector).flat_map(|x| x.text()).collect();
             let urls: Vec<&str> = document
-                .select(&url_selector)
+                .select(&selector)
                 .map(|x| x.value().attr("href").unwrap())
                 .collect();
-            for number in 0..min(titles.len(), 5) {
+            for (number, title) in titles.iter().enumerate().take(min(titles.len(), 5)) {
                 println!(
                     "\n{} {}\n",
                     format!("{}", number).cyan().bold(),
-                    titles[number].green()
+                    title.green()
                 )
             }
             print!("Enter selection (n for new search): ");
@@ -67,17 +59,20 @@ fn main() {
             stdin()
                 .read_line(&mut selection)
                 .expect("Failed to read line");
-            if &selection.trim() == &"n" {
-                continue;
-            } else if &selection.trim() == &"" {
+            if (selection.trim() == "n") || (selection.trim() == "") {
                 continue;
             }
             let selection: usize = selection.trim().parse().expect("Please type a number.");
             let split: Vec<&str> = urls[selection].split("?v=").collect();
             let id = split[1];
+            println!(
+                "\n{} {}",
+                "Now playing:".magenta().bold(),
+                format!("https://youtube.com/watch?v={}\n", id).yellow()
+            );
             Command::new("mpv")
                 .arg(format!(
-                    "https://vid.puffyan.us/latest_version?id={}&itag=251&local=true",
+                    "https://iv.melmac.space/latest_version?id={}&itag=251&local=true",
                     id
                 ))
                 .status()
